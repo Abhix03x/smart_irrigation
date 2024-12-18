71540fc11f1fdb22b96b0c21e164c34eec1e33d1
Abhinand <abhinandatr@gmail.com>
2024-12-18 19:42:56 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const handleCick = () => {
-    return <h2>Oombikkoo...</h2>
+  const[text,setText] = useState(false);
+  function handleCick(){
+    setText(!text);
   }
   return (
     <>
      <h1>Hello Madaa</h1>
      <button onClick={handleCick}>Click Here</button>
+     {
+      text && <h2>Ooombikko....</h2>
+     }
     </>
   )
 }
